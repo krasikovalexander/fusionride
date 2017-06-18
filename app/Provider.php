6fbd9b5aab2d72330708845802b944d68bc1ef6e
krasikovap <krasikovap@gmail.com>
2017-06-18 14:03:28 +0600

    logging

--- app/Provider.php
+++ app/Provider.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Log;
 
 class Provider extends Model
 {
